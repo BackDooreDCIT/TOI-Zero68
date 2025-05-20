bd75acfc1a94a700cd17c1eaad4c33564d2884ed
BackDoore <noempang.parattakorn@gmail.com>
2025-05-20 20:12:01 +0700

    A1-009 & A1-010 Finished

---
+++ A1/A1-009/A1-009.py
@@ -0,0 +1,8 @@
+score1 = int(input())
+score2 = int(input())
+total = score1+score2
+print(total)
+if total < 50:
+    print("fail")
+else:
+    print("pass")
---
+++ A1/A1-010/A1-010.py
@@ -0,0 +1,6 @@
+age = int(input())
+char = input()
+if age < 18 or (char == "s" or char == "S"):
+    print("20")
+else:
+    print("50")
