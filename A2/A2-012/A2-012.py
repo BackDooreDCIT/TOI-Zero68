ce019bc1a83151a10802b6efff1f0e3178ba0d87
BackDoore <noempang.parattakorn@gmail.com>
2025-05-25 17:14:53 +0700

    A2-012 Finished

---
+++ A2/A2-012/A2-012.py
@@ -0,0 +1,9 @@
+item1 = 10
+item2 = 25
+item3 = 3
+inp = input().split()
+amount1 = int(inp[0])
+amount2 = int(inp[1])
+amount3 = int(inp[2])
+total = (item1*amount1) + (item2*amount2) + (item3*amount3)
+print(total)
