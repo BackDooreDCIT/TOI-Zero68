0a4e4b074bef863d00d11ade2b4d56315a5de56f
BackDoore <noempang.parattakorn@gmail.com>
2025-05-26 19:44:50 +0700

    A3-026 Finished

---
+++ A3/A3-026/A3-026.py
@@ -0,0 +1,33 @@
+mapsize = input().split()
+looptime = int(mapsize[0])
+map1 = []
+map2 = []
+
+for i in range(looptime):
+    inp = input()
+    map1.append(list(inp.strip()))
+
+# print('map1:', map1)
+# print(map1)
+
+for i in range(looptime):
+    inp = input()
+    map2.append(list(inp.strip()))
+
+# print('map2:', map2)
+print('=========')
+
+for i in range(looptime):
+    for j in range(len(map1[i])):
+        if map1[i][j] == "-" and map2[i][j] == "-":
+            continue
+        elif (map1[i][j] == "-" and map2[i][j] == "+") or (map2[i][j] == "-" and map1[i][j] == "+"):
+            map1[i][j] = "+"
+        elif (map1[i][j] == "-" and map2[i][j] == "x") or (map2[i][j] == "-" and map1[i][j] == "x"):
+            map1[i][j] = "x"
+        elif (map1[i][j] == "+" and map2[i][j] == "x") or (map2[i][j] == "+" and map1[i][j] == "x"):
+            map1[i][j] = "*"
+
+for i in range(looptime):
+        print(*map1[i], sep="")
+
