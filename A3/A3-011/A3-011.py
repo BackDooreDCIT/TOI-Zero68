3783fd48d5ac77185d6c5347ba5f2b95de0e3d78
BackDoore <noempang.parattakorn@gmail.com>
2025-05-29 16:38:02 +0700

    A3-011 Finished

---
+++ A3/A3-011/A3-011.py
@@ -0,0 +1,18 @@
+storeamount = int(input())
+prices = input().split()
+allpossible = []
+curprice = []
+for i in range(storeamount):
+    curprice = prices[i:len(prices)]
+    # print(curprice)
+    for j in range(len(curprice)):
+        if j == 0:
+            pass
+        else:
+            curprice[j] = int(curprice[j]) + int(curprice[j-1])
+    for j in range(len(curprice)):
+        if int(curprice[j]) not in allpossible:
+            allpossible.append(int(curprice[j]))
+        else:
+            pass
+print(len(allpossible))
